12ed500e69ebffcff47d2eee180e3dd103d7cef6
Aditya Madhab <130239261+adityamadhab@users.noreply.github.com>
2024-05-24 11:51:14 +0530

    Update AboutPage.jsx

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -5,14 +5,14 @@ export function AboutPage() {
     return (
         <div>
             <div className="flex flex-col md:flex-row items-center w-screen h-auto bg-white mb-8 p-8 md:p-16">
-                <div className="md:w-1/2 flex justify-center md:justify-end order-1 md:order-none mt-8 md:mt-0">
+                <div className="w-full md:w-1/2 flex justify-center mt-8 md:mt-0">
                     <img
                         src="/about.jpg"
                         alt="About Us"
                         className="w-full max-w-xs md:max-w-md rounded-lg"
                     />
                 </div>
-                <div className="md:w-1/2 text-left flex flex-col justify-center md:justify-evenly order-none md:order-1">
+                <div className="w-full md:w-1/2 text-left flex flex-col justify-center md:justify-evenly mt-8 md:mt-0">
                     <h1 className="text-3xl md:text-5xl text-yellow-500 mb-10 md:mb-20 font-black">
                         ABOUT US
                     </h1>
